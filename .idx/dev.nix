a30ebca8a1ebc8829de90dd85e1dbcd22c8e3cdc
chilensis.dev <chilensis.dev@gmail.com>
2025-03-30 13:47:37 +0000

    Add IDX Setting

---
+++ .idx/dev.nix
@@ -0,0 +1,40 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.pnpm
+    pkgs.nodejs-18_x
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = ["astro-build.astro-vscode"];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        build-docs = ''
+          pnpm install
+        '';
+      };
+
+      onStart = {
+          build-docs = "pnpm run --filter docs build";
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = { 
+          command = [ "pnpm" "--filter" "docs" "dev" "--" "--port" "9008" "--hostname" "0.0.0.0" ];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
