8f7a996654082dd6082a82ac0c35caab69a35779
chilensis.dev <chilensis.dev@gmail.com>
2025-03-30 13:47:28 +0000

    Add Docs Settings

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,24 +1,25 @@
 import starlight from '@astrojs/starlight';
 import { defineConfig } from 'astro/config';
-import page from '@pelagornis/page';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://pelagornis.github.io/page/',
-    integrations: [
-      starlight({
-        logo: {
-          dark: './src/assets/logo-dark.svg',
-          light: './src/assets/logo-light.svg',
-          alt: 'Starlight Page',
-        },
-        editLink: {
-          baseUrl: 'https://github.com/pelagornis/page/edit/main/docs/',
-        },
-        plugins: [
-          page()
-        ],
-      }),
-    ],
-  })
-  
+  site: 'https://starlight-theme-black.vercel.app/',
+  integrations: [
+    starlight({
+      logo: {
+        dark: './src/assets/logo-dark.svg',
+        light: './src/assets/logo-light.svg',
+        alt: 'Starlight Page',
+      },
+      editLink: {
+        baseUrl: 'https://github.com/pelagornis/starlight-theme-page/edit/main/docs/',
+      },
+      plugins: [
+      ],
+      social: {
+        github: 'https://github.com/pelagornis/starlight-theme-page',
+      },
+      title: 'starlight/page',
+    }),
+  ],
+})
